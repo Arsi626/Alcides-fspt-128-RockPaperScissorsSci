843864979b9f022183b25ddc77e5a006065f33b5
Alcides Rivera <elarsi626@gmail.com>
2025-11-21 19:21:32 +0000

    First

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
-import "bootstrap";
-import "./style.css";
+const choises = ["rock", "paper", "scissors", "lizard", "spock"];
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const rules = {
+  rock: ["scissors", "lizard"],
+  paper: ["rock", "spock"],
+  scissors: ["paper", "lizard"],
+  lizard: ["spock", "paper"],
+  spock: ["scissors", "rock"]
 };
+
+const jugar = (userChoise) => {
+  function getRandomNumber(minimo, maximo) {
+    return Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
+  }
+  const computerChoise = getRandomNumber(0, 4)
+  const eleccion = choises[computerChoise];
+
+  console.log("User chose:", userChoise);
+  console.log("Computer chose:", eleccion);
+
+    if (userChoise === eleccion){
+      console.log("Draw");
+    }
+    else if (rules[userChoise].includes(eleccion)){
+      console.log("You win!");
+    }
+    else {
+      console.log("Computer Wins!");
+    }
+}
+
+
+jugar("lizard");
+
